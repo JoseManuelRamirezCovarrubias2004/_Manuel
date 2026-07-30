627baec420e244f80ed920afe5d9f0b562948f10
Reynaldo003 <rvallejo276@gmail.com>
2026-07-29 18:06:02 -0600

    nueva interfaz rendimiento digitale

--- src/pages/Comercial/ComercialTopNav.jsx
+++ src/pages/Comercial/ComercialTopNav.jsx
@@ -54,6 +54,8 @@ export default function ComercialTopNav() {
 
     const canSeeRendimiento = hasAnyPermission([
         "USUARIOS_ADMIN",
+        "CRM_COORDINADOR_DIGITAL",
+        "CRM_DIGITALES",
     ]);
 
     const tabs = useMemo(() => {
